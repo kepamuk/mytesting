995281d0744be2788d53ba8aea2fe8c1e3f4907e
kepamuk <kepamuk@yandex.ru>
2018-02-04 22:11:58 +0300

    9_lesson

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../shared/model/user.model';
+import {UsersService} from '../../shared/services/users.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  constructor(private usersService: UsersService,
+              private router: Router) {
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      'email': new FormControl('stas@stas.ru', [Validators.required, Validators.email]),
+      'password': new FormControl('12345678', [Validators.required, Validators.minLength(6)]),
+      'name': new FormControl('stas', [Validators.required]),
+      'agree': new FormControl(true, [Validators.requiredTrue])
+    });
+  }
+
+  onSubmit() {
+    const {email, password, name} = this.form.value;
+    const user = new User(email, password, name);
+
+    this.usersService.createNewUser(user)
+      .subscribe(() => {
+        this.router.navigate(['/login'], {
+          queryParams: {
+            nowCanLogin: true
+          }
+        });
+      });
   }
 
 }
