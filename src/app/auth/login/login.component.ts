a4fba5e04ccc670a0a6d3256245ee017c0d5236c
kepamuk <kepamuk@yandex.ru>
2018-02-04 15:11:22 +0300

    7_lesson

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {UsersService} from '../../shared/services/users.service';
+import {Message} from '../../shared/model/message.model';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +11,41 @@ import {FormControl, FormGroup, Validators} from '@angular/forms';
 export class LoginComponent implements OnInit {
 
   form: FormGroup;
+  message: Message;
 
-  constructor() {
-    this.form = new FormGroup({
-      email: new FormControl('', [Validators.required, Validators.email]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
-    });
+  constructor(private usersService: UsersService) {
+
   }
 
   ngOnInit() {
+    this.message = new Message('danger', '');
+    this.form = new FormGroup({
+      email: new FormControl('wfm@mail.ru', [Validators.required, Validators.email]),
+      password: new FormControl('12345678', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  private showMessage(text: string, type: string = 'danger') {
+    this.message = new Message(type, text);
+    window.setTimeout(() => {
+      this.message = new Message(type, '');
+    }, 3000);
   }
 
   onSubmit() {
-    console.log(this.form);
+    const values = this.form.value;
+    this.usersService.getUserByEmail(values.email)
+      .subscribe((user) => {
+        if (user) {
+          if (user.password === values.password) {
+            console.log(true);
+          } else {
+            this.showMessage('no pass');
+          }
+        } else {
+          this.showMessage('no user');
+        }
+      });
   }
 
 }
---
+++ src/app/shared/model/message.model.ts
@@ -0,0 +1,5 @@
+export class Message {
+  constructor(private type: string,
+              private text: string) {
+  }
+}
